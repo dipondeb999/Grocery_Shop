bf064173c33ba4d2f51bfac170cc84b24608219f
dipondeb <debdipon37@gmail.com>
2024-07-14 01:02:10 +0600

    updated

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import '../model/cart_model.dart';
 
@@ -8,59 +9,127 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        title: const Text(
-            'Add Cart',
-          style: TextStyle(
-              color: Colors.white,
-          ),
-        ),
-        leading: IconButton(
-          onPressed: (){
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-              Icons.arrow_back_ios,
-            color: Colors.white,
-          ),
-        ),
-      ),
-      body: Consumer<CartModel>(
-        builder: (context, value, child){
-          return Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  padding: const EdgeInsets.all(12),
-                  itemCount: value.cartItems.length,
-                    itemBuilder: (context,index){
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: ListTile(
-                          leading: Image.asset(
-                              value.cartItems[index][2],
-                            height: 36,
-                          ),
-                          title: Text(value.cartItems[index][0]),
-                          subtitle: Text('\$' + value.cartItems[index][1]),
-                          trailing: IconButton(
-                            onPressed: () => Provider.of<CartScreen> (context, listen: false),
-                              icon : const Icon(Icons.cancel)
-                          ),
-                        ),
-                      );
-                    }
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.black,
                 ),
-              ),
-            ],
-          );
-        },
-      )
+            ),
+          ),
+        ),
+        body: Consumer<CartModel>(
+          builder: (context, value, child) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 24, top: 5),
+                  child: Text(
+                    "My Cart",
+                    style: GoogleFonts.notoSerif(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      padding: const EdgeInsets.all(12),
+                      itemCount: value.cartItems.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: ListTile(
+                            leading: Image.asset(
+                              value.cartItems[index][2],
+                              height: 36,
+                            ),
+                            title: Text(value.cartItems[index][0]),
+                            subtitle: Text('\$' + value.cartItems[index][1]),
+                            trailing: IconButton(
+                                onPressed: () => Provider.of<CartScreen>(context, listen: false),
+                                icon: const Icon(Icons.cancel)
+                            ),
+                          ),
+                        );
+                      }
+                  ),
+                ),
+                // total + pay now
+                Padding(
+                  padding: const EdgeInsets.all(36),
+                  child: Container(
+                    padding: const EdgeInsets.all(24),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // price
+                       Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: [
+                           Text(
+                             'Total price',
+                             style: TextStyle(
+                                 color: Colors.green[100]
+                             ),
+                           ),
+                           const SizedBox(height: 4),
+                           Text(
+                             value.calculateTotal(),
+                             style: const TextStyle(
+                               color: Colors.white,
+                               fontSize: 18,
+                               fontWeight: FontWeight.bold,
+                             ),
+                           ),
+                         ],
+                       ),
+                        // pay now button
+                        Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.green.shade100),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Row(
+                            children: [
+                              Text(
+                                'Pay Now',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                                  color: Colors.white,
+                              ),
+                            ],
+                          )
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        )
     );
   }
 }
